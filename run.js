38d603e8f2f2f1a5b08a10d8656ce22a226f19d6
Ryan Shaw <ryanshaw@unc.edu>
2020-05-16 09:58:04 -0400

    troubleshooting

--- run.js
+++ run.js
@@ -11,7 +11,7 @@ let testcafe = null
 
 const browsers = [{
   path: '/usr/bin/google-chrome-stable',
-  cmd: '--disable-gpu',
+  cmd: '--headless --disable-gpu',
 }]
 
 if (platform() === 'darwin') {
