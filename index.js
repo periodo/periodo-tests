6e438abd96dd6249c5aa3a41ef52730e4bc9090c
Ryan Shaw <ryanshaw@unc.edu>
2020-05-16 17:35:37 -0400

    try to run all tests on chrome

--- index.js
+++ index.js
@@ -12,58 +12,58 @@ test('First breadcrumb should say Canonical', async t => {
   await t.expect(page.firstBreadcrumb.innerText).eql('Canonical')
 })
 
-// test('After filtering by label, first row label should match', async t => {
-//   await page.setLabelFilter('bronze')
-//   await t
-//     .expect(page.periodList.firstRow.label.textContent)
-//     .contains('Bronze')
-// })
+test('After filtering by label, first row label should match', async t => {
+  await page.setLabelFilter('bronze')
+  await t
+    .expect(page.periodList.firstRow.label.textContent)
+    .contains('Bronze')
+})
 
-// test('Periods starting < 50000BC should be filtered by default', async t => {
-//   await t
-//     .expect(page.periodList.periodsShown.textContent)
-//     .match(/^[1-9]\d* periods$/, { timeout: 12000 }) // >0 periods shown
-//     .expect(page.periodList.periodsFiltered.textContent)
-//     .match(/^[1-9]\d* periods not shown/)            // >0 periods filtered
-// })
+test('Periods starting < 50000BC should be filtered by default', async t => {
+  await t
+    .expect(page.periodList.periodsShown.textContent)
+    .match(/^[1-9]\d* periods$/, { timeout: 12000 }) // >0 periods shown
+    .expect(page.periodList.periodsFiltered.textContent)
+    .match(/^[1-9]\d* periods not shown/)            // >0 periods filtered
+})
 
-// test('Widening the time filter should show all the periods', async t => {
-//   await page.setWidestTimeFilter()
-//   await t
-//     .expect(page.periodList.periodsShown.textContent)
-//     .match(/^[1-9]\d* periods$/)           // >0 periods shown
-//     .expect(page.periodList.periodsFiltered.textContent)
-//     .match(/^Click a period to select it/) // no periods filtered
-// })
+test('Widening the time filter should show all the periods', async t => {
+  await page.setWidestTimeFilter()
+  await t
+    .expect(page.periodList.periodsShown.textContent)
+    .match(/^[1-9]\d* periods$/)           // >0 periods shown
+    .expect(page.periodList.periodsFiltered.textContent)
+    .match(/^Click a period to select it/) // no periods filtered
+})
 
-// test('After filtering by place, first row label should match', async t => {
-//   await page.setPlaceFilter('denmark')
-//   await t
-//     .expect(page.periodList.firstRow.spatialCoverage.textContent)
-//     .contains('Denmark')
-// })
+test('After filtering by place, first row label should match', async t => {
+  await page.setPlaceFilter('denmark')
+  await t
+    .expect(page.periodList.firstRow.spatialCoverage.textContent)
+    .contains('Denmark')
+})
 
-// test('Selecting a facet value should update other facets', async t => {
-//   await page.facets['authority']
-//     .selectValue('Norsk arkeologisk leksikon. 2005.')
+test('Selecting a facet value should update other facets', async t => {
+  await page.facets['authority']
+    .selectValue('Norsk arkeologisk leksikon. 2005.')
 
-//   await t
-//     .expect(page.facets['language'].values.count)
-//     .eql(1)
-//     .expect(page.facets['language'].values.textContent)
-//     .eql('Norwegian')
+  await t
+    .expect(page.facets['language'].values.count)
+    .eql(1)
+    .expect(page.facets['language'].values.textContent)
+    .eql('Norwegian')
 
-//   await t
-//     .expect(page.facets['spatialCoverage'].values.count)
-//     .eql(1)
-//     .expect(page.facets['spatialCoverage'].values.textContent)
-//     .eql('Norway')
-// })
+  await t
+    .expect(page.facets['spatialCoverage'].values.count)
+    .eql(1)
+    .expect(page.facets['spatialCoverage'].values.textContent)
+    .eql('Norway')
+})
 
-// test('Clicking a row should show its details below', async t => {
-//   await page.periodList.firstRow.click()
-//   const label = await page.periodDetails.originalLabel()
-//   await t
-//     .expect(page.periodList.firstRow.label.textContent)
-//     .eql(label.textContent)
-// })
+test('Clicking a row should show its details below', async t => {
+  await page.periodList.firstRow.click()
+  const label = await page.periodDetails.originalLabel()
+  await t
+    .expect(page.periodList.firstRow.label.textContent)
+    .eql(label.textContent)
+})
